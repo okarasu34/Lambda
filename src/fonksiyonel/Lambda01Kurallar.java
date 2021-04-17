b00737f9af5c97fdea0449bc9b4915fd5c240f6a
okarasu34 <ozckrs34@gmail.com>
2021-04-17 15:55:51 +0200

    Initial commit

---
+++ src/fonksiyonel/Lambda01Kurallar.java
@@ -0,0 +1,21 @@
+package fonksiyonel;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.List;
+
+public class Lambda01Kurallar {
+
+
+    public static void main(String[] args) {
+
+        List<Integer> Liste= Arrays.asList(1,2,3,4,5);
+        Liste.forEach(x-> System.out.println(x));
+
+
+
+    }
+
+
+
+}
