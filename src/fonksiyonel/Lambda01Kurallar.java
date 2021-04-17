c93f2ea22c50235a63809ab518f6375b7d0ba1e1
okarasu34 <ozckrs34@gmail.com>
2021-04-17 16:38:40 +0200

    son version

--- src/fonksiyonel/Lambda01Kurallar.java
+++ src/fonksiyonel/Lambda01Kurallar.java
@@ -11,6 +11,16 @@ public class Lambda01Kurallar {
 
         List<Integer> Liste= Arrays.asList(1,2,3,4,5);
         Liste.forEach(x-> System.out.println(x));
+        System.out.println("====================================================");
+    //Listenin elemanlarini 2 arttirarak yazdiralim.
+        System.out.println("Eger satir sayisi birden fazlaysa suslu parantez kullanmaliyiz");
+        System.out.println("eger veri tipikullanilirsa explicit olur");
+    Liste.forEach(t-> System.out.print(t+2+" "));
+Liste.forEach(t->{
+    int miktar=2;
+     System.out.print(t+2+" ");
+});
+
 
 
 
